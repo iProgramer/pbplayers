4a8d53a82b2e04fa8b3fbd1f4580b5689346ebd8
Simon Poloczek <Simon.Poloczek@sevdesk.de>
2020-02-24 17:43:57 +0100

    added failing test

--- src/test/kotlin/de/spoloczek/pbplayers/PbplayersApplicationTests.kt
+++ src/test/kotlin/de/spoloczek/pbplayers/PbplayersApplicationTests.kt
@@ -1,5 +1,6 @@
 package de.spoloczek.pbplayers
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,8 @@ class PbplayersApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assertions.assertTrue(false);
 	}
+
 
 }
