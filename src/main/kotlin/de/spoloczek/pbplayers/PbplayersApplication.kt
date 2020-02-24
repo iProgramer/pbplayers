5b481e7ddf9f9437abbd293f1cc8559bf74eab72
Simon Poloczek <Simon.Poloczek@sevdesk.de>
2020-02-24 17:24:11 +0100

    initial Commit

---
+++ src/main/kotlin/de/spoloczek/pbplayers/PbplayersApplication.kt
@@ -0,0 +1,11 @@
+package de.spoloczek.pbplayers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PbplayersApplication
+
+fun main(args: Array<String>) {
+	runApplication<PbplayersApplication>(*args)
+}
---
+++ src/test/kotlin/de/spoloczek/pbplayers/PbplayersApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.spoloczek.pbplayers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PbplayersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
